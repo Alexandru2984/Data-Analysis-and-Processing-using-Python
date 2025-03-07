ba3646a796ca2cddc9be7d649d67395aa1b9a934
Dragne Alexandru Mihai <alex_mihai984@yahoo.com>
2025-03-07 20:52:10 +0200

    Django numpy si site dinamic

--- CURS_1/15.exercitiul_cu_ani.py
+++ CURS_1/15.exercitiul_cu_ani.py
@@ -1,5 +1,14 @@
 import numpy as np
 
-startDate = 1980
-endDate = 2030
+startDate = 1899
+endDate = 2025
+##Este bisect daca este divizibil cu 400
+##Este divizibil cu 4 dar nu cu 100
 
+array = np.arange(startDate, endDate+1)
+
+divizibile_cu_400 = array % 400 == 0
+divizibile_cu_4 = array % 4 == 0
+nedivizibile_cu_100 = array % 100 != 0
+
+print(array[divizibile_cu_400 | (divizibile_cu_4 & nedivizibile_cu_100)])
---
+++ CURS_1/edvefrvre.py
@@ -0,0 +1,6 @@
+import numpy as np
+
+startDate = 1899
+endDate = 2025
+
+array = np.arange[startDate, endDate+1]
